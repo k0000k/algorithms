8b9924bb162c3ac06a60670e771144bcb92da06e
HyeGyeong Kook <84006880+k0000k@users.noreply.github.com>
2024-01-24 17:19:45 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = n; i > 0; i--) {
+        cout << i << endl;
+    }
+
+    return 0;
+}

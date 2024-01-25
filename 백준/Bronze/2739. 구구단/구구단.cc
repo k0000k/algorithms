f35d86d3a76eac0712256ccfa5ed4da08768883f
HyeGyeong Kook <84006880+k0000k@users.noreply.github.com>
2024-01-25 22:46:17 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= 9; i++) {
+        cout << n << " * " << i << " = " << n * i << endl;
+    }
+
+    return 0;
+}

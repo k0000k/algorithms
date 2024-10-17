50101983fb287a33b2b699511751b1e97b32c13f
HyeGyeong Kook <84006880+k0000k@users.noreply.github.com>
2024-10-17 23:22:49 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,19 +1,21 @@
 import sys
 
-board = []
+board=[]
+
 for i in range(9):
-    temp = list(map(int, sys.stdin.readline().rstrip().split()))
+    temp=list(map(int,sys.stdin.readline().rstrip().split()))
     board.append(temp)
 
-max_value = -1
-max_i = -1
-max_j = -1
+max_val=-1
+max_i=0
+max_j=0
+
 for i in range(9):
     for j in range(9):
-        if (board[i][j] > max_value):
-            max_value = board[i][j]
-            max_i = i
-            max_j = j
+        if board[i][j]>max_val:
+            max_val=board[i][j]
+            max_i=i
+            max_j=j
 
-print(max_value)
-print(max_i + 1, max_j + 1)
+print(max_val)
+print(max_i+1,max_j+1)
